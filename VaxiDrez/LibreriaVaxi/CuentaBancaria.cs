351df3d0354913fd8ab1e6fb2d04d96d3dc8cada
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-01-25 09:10:08 -0400

    Creacion de ILoggerGeneral

--- VaxiDrez/LibreriaVaxi/CuentaBancaria.cs
+++ VaxiDrez/LibreriaVaxi/CuentaBancaria.cs
@@ -8,26 +8,34 @@ namespace LibreriaVaxi
 {
     public class CuentaBancaria
     {
+        //inyeccion DP
+        private readonly ILoggerGeneral _loggerGeneral;
+
+        //propiedad
         public int balance { get; set; }
 
-        public CuentaBancaria() 
+        public CuentaBancaria(ILoggerGeneral loggerGeneral) 
         {
+            _loggerGeneral = loggerGeneral;
             balance = 0;
         }
         public bool Deposito(int monto)
         {
+            _loggerGeneral.Message("EL Monto a Depositar es: " + monto);
             balance += monto;
             return true;
         }
         public bool Retiro(int monto) 
         {
-            if (balance => monto) 
+            if (balance >= monto) 
             {
+                _loggerGeneral.Message("EL monto a retirar es: " + monto);
                 balance -= monto;
                 return true;
             }
             else 
             {
+                _loggerGeneral.Message("No tiene dinero suficiente, su monton en menor a "+ monto);
                 return false;
             }
         }
---
+++ VaxiDrez/LibreriaVaxi/LoggerGeneral.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaVaxi
+{
+    public interface ILoggerGeneral 
+    {
+        void Message(string message);
+    }
+    public class LoggerGeneral : ILoggerGeneral
+    {
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
