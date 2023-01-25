091e318dae87e927cef21930da2d29de7dcd2359
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-01-25 09:59:10 -0400

    Video 22. Implementacion de mocking

--- VaxiDrez/LibreriaVaxiNUnitTest/CuentaBancariasNUnitTest.cs
+++ VaxiDrez/LibreriaVaxiNUnitTest/CuentaBancariasNUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,12 @@ namespace LibreriaVaxi
     [TestFixture]
     public class CuentaBancariasNUnitTest
     {
-        private CuentaBancaria cuentaBancaria;
+        private CuentaBancaria cuenta;
 
         [SetUp]
         public void SetUp()
         {
-            cuentaBancaria = new CuentaBancaria(new LoggerFake());
+            
         }
 
         [Test]
@@ -37,6 +38,37 @@ namespace LibreriaVaxi
             #endregion
 
             //Arrage
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(new LoggerFake());
+
+            //Act
+            var resultado = cuentaBancaria.Deposito(100);
+
+            //Assert
+            Assert.True(resultado);
+            Assert.That(cuentaBancaria.GetBalance, Is.EqualTo(100));
+        }
+        [Test]
+        public void Deposito_InputMonto100Mock_returnTrue()
+        {
+            #region Firma
+            /* 
+             * =============================================
+             *  Author:          Enderson Florian Solan
+             *  Author modified: null
+             *  Create date:     null
+             *  Tester:          null
+             *  Description:
+             *      Metodo que para depositar monto en cuenta
+             *      dentro de la empresa, si se deposita retorna
+             *      verdadero.
+             * =============================================
+             */
+            #endregion
+
+            var mocking = new Mock<ILoggerGeneral>();
+
+            //Arrage
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(mocking.Object);
             //Act
             var resultado = cuentaBancaria.Deposito(100);
 
